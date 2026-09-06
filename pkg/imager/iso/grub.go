3ce73bc9b3ca886c23dfee2beca201cfbac1e82a
Eleanor Young <developer@nausicaea.net>
2026-09-06 18:06:26 +0200

    fix(imager): grub-mkrescue understands only the --fonts argument, not --font

--- pkg/imager/iso/grub.go
+++ pkg/imager/iso/grub.go
@@ -77,7 +77,7 @@ func (options Options) CreateGRUB(printf func(string, ...any)) (Generator, error
 			"--compress=xz",
 			"--output=" + options.OutPath,
 			"--verbose",
-			"--font=",
+			"--fonts=",
 			options.ScratchDir,
 			"-iso-level", "3",
 			"--",
